557404338ed1a13a7b7b2a8e2d9cd11f068e142a
yaohu <yaohu201203@gmail.com>
2016-07-28 14:55:40 +0800

    add

--- Kindergartern/app/src/main/java/com/junbaole/kindergartern/presentation/base/BaseActivity.java
+++ Kindergartern/app/src/main/java/com/junbaole/kindergartern/presentation/base/BaseActivity.java
@@ -1,11 +1,14 @@
 package com.junbaole.kindergartern.presentation.base;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.junbaole.kindergartern.R;
 
@@ -16,6 +19,14 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //透明状态栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            // Translucent status bar
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
     }
 
 }
